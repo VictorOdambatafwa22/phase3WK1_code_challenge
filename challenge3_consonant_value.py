ad8654d4ba4aae4aec9ae420b7237537af48431e
victor odambatafwa <omondivictoras@gmail.com>
2023-08-19 21:35:34 +0300

    Updated Challenge3

--- challenge3_consonant_value.py
+++ challenge3_consonant_value.py
@@ -1,4 +1,24 @@
-def highest_value_of_consonant_substrings(consonant):
- 
-    pass
+
+def consonants(string):
+    consonants = []
+
+    alph=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    for char in string:
+        
+        if char.lower() not in ['a', 'e', 'i', 'o', 'u']:
+
+            char2=alph.index(char)
+            
+            consonants.append(char2)
+
+
+
+    print (max(consonants))      
+    return max(consonants)
+   
+    
+
+consonants('howareyou')
+
 
